1a7bd47d8d1847ef992dea38cd4ea5c24724870f
Daria <dariagnaumova@gmail.com>
2020-01-18 11:53:50 -0500

    script links and button id added

--- assets/script.js
+++ assets/script.js
@@ -9,10 +9,10 @@ var wineKEY = "4e2cbd32ae6b47f3acb6348b6fb258f6";
     function wineDataLoad(){
       
       var wineURL = "https://api.spoonacular.com/food/wine/dishes?wine="
-      var userInput = $("input").val().trim();
+      var userInput = $("#userInput").val().trim();
 
         $.ajax({
-          url: wineURL + userInput + wineKEY,
+          url: wineURL + userInput,
           method: "GET"
       }).then(function (wineRes) {
         console.log(wineRes)
