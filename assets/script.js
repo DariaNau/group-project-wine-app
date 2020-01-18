e8a10707b2f6b463b43597df1eb52b8fa2f5ac7f
Daria <dariagnaumova@gmail.com>
2020-01-18 11:34:15 -0500

    wine url and key added

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,24 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
-  
-    $("button").click(function(){
-      $.ajax({url: , 
-        success: function(result){
-        $("div").html(result);
-      }});
+var wineKEY = "4e2cbd32ae6b47f3acb6348b6fb258f6";
+
+
+    $("#searchBtn").click(function(){
+      wineDataLoad();
+
     });
-  });
+    
+    function wineDataLoad(){
+      
+      var wineURL = "https://api.spoonacular.com/food/wine/dishes?wine="
+      var userInput = $("input").val().trim();
+
+        $.ajax({
+          url: wineURL + userInput + wineKEY,
+          method: "GET"
+      }).then(function (wineRes) {
+        console.log(wineRes)
+      });
+    };
+
+
+
+  
