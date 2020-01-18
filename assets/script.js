c5dc1405ef6fa241d712c2acd5de9c0a654fd56d
Daria <dariagnaumova@gmail.com>
2020-01-18 14:18:19 -0500

    appending works

--- assets/script.js
+++ assets/script.js
@@ -15,14 +15,23 @@ var wineKEY = "4e2cbd32ae6b47f3acb6348b6fb258f6";
           url: wineURL,
           method: "GET"
       }).then(function (wineRes) {
-        console.log(wineRes.text)
+
+        console.log(wineRes)
         
-        var wineINFO = document.createElement("div");
-        wineINFO.html = (wineRes.text);
+        var response1 = (wineRes.text);
+        var wineINFO = $("#wineDiv");
+        var p = $("<p></p>").text("Wine description: " + response1);
+        wineINFO.html(p);
 
-        var foodINFO = document.createElement("div1");
-        foodINFO.html = (wineRes.pairings);
+        var response2 = (wineRes.pairings);
+        var foodINFO = $("#recipeDiv");
 
+        foodINFO.empty();
+        for (var i = 0; i < response2.length; i++){
+          var li = $("<li></li>").text("Option "+ [i+1] + ": " + response2[i]);
+          foodINFO.append(li);
+        };
+        $("#userInput").val("");
       });
     };
 
