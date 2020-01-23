0a884b177d8754e936675a04c89b498bbe5db200
Daria <dariagnaumova@gmail.com>
2020-01-23 18:06:40 -0500

    algolia

--- assets/js/algolia.js
+++ assets/js/algolia.js
@@ -1,26 +1,26 @@
 
-// const config = {
-//   APPLICATION_ID: 'U4CQ51CWWF',
-//   SEARCH_ONLY_API_KEY: 'ab00b914a80138bc9888ba615f0bf965',
-// };
+const config = {
+  APPLICATION_ID: 'U4CQ51CWWF',
+  SEARCH_ONLY_API_KEY: 'ab00b914a80138bc9888ba615f0bf965',
+};
 
-// const client = algoliasearch(config.APPLICATION_ID, config.SEARCH_ONLY_API_KEY);
-// const index = client.initIndex('PROJECT_1');
+const client = algoliasearch(config.APPLICATION_ID, config.SEARCH_ONLY_API_KEY);
+const index = client.initIndex('PROJECT_1');
 
 
-// autocomplete('#userInput', { hint: true }, [
-//   {
-//     source: autocomplete.sources.hits(index, { hitsPerPage: 5 }),
-//     displayKey: 'strDrink',
-//     templates: {
-//       suggestion: function (suggestion) {
-//         return suggestion._highlightResult.strDrink.value;
-//       }
-//     }
-//   }
-// ]).on('autocomplete:selected', function (event, suggestion, dataset, context) {
-//   console.log(event, suggestion, dataset, context);
-// });
+autocomplete('#userInput', { hint: true }, [
+  {
+    source: autocomplete.sources.hits(index, { hitsPerPage: 5 }),
+    displayKey: 'strDrink',
+    templates: {
+      suggestion: function (suggestion) {
+        return suggestion._highlightResult.strDrink.value;
+      }
+    }
+  }
+]).on('autocomplete:selected', function (event, suggestion, dataset, context) {
+  console.log(event, suggestion, dataset, context);
+});
 
 /* index.search({ query: 'drinks' }, function (data) {
   console.log(data)
