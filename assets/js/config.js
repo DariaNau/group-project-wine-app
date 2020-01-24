c2b629bd49450025411cae7bb5c7f88ee9c3a5d8
Daria <dariagnaumova@gmail.com>
2020-01-23 20:21:24 -0500

    test

--- assets/js/config.js
+++ assets/js/config.js
@@ -5,7 +5,6 @@ const config = {
   };
   const client = algoliasearch(config.APPLICATION_ID, config.ADMIN_KEY);
   const index = client.initIndex('PROJECT_1');
-
   const drinks = 
       [
       {
@@ -63,9 +62,7 @@ const config = {
       {
         "strDrink": "Zinfandel"
       }
-    ]
-  
-
+    ];
   index.addObjects(drinks, (err, content) => {
       if (err) {
           console.log(err)
@@ -85,50 +82,3 @@ const config = {
   ]).on('autocomplete:selected', function (event, suggestion, dataset, context) {
     console.log(event, suggestion, dataset, context);
   });
-//   $.get(
-//     "wine-data.json"
-//   ).then(res => {
-//     console.log(res.drinks);
-//     const client = algoliasearch(
-//       'U4CQ51CWWF',
-//       'ab00b914a80138bc9888ba615f0bf965'
-//     );
-//     const drinkArr = [];
-//     const index = client.initIndex('dev_DRINKS');
-//     const drinkObj = res.drinks;
-//     // console.log('typeof', typeof drinkObj);
-//     // console.log('keys', Object.keys(drinkObj));
-//     // console.log("drinkObj['0']", drinkObj['0']);
-//     Object.keys(drinkObj).forEach(key => {
-//       drinkArr.push(drinkObj[key]);
-//     });
-//     // console.log('drinkArr', drinkArr);
-//     // console.log('typeof drinkArr', typeof drinkArr);
-//     index.addObjects(drinkObj, (err, content) => {
-//       if (err) {
-//         console.error('err', err);
-//       }
-//       console.log(content);
-//     });
-//     index.setSettings(
-//       {
-//         searchableAttributes: ['strDrink']
-//       },
-//       (err, content) => {
-//         console.log(content);
-//       }
-//     );
-//     autocomplete('#userInput', { hint: false }, [
-//       {
-//         source: autocomplete.sources.hits(index, { hitsPerPage: 5 }),
-//         displayKey: 'my_attribute',
-//         templates: {
-//           suggestion: function(suggestion) {
-//             return suggestion._highlightResult.my_attribute.value;
-//           }
-//         }
-//       }
-//     ]).on('autocomplete:selected', function(event, suggestion, dataset, context) {
-//       console.log(event, suggestion, dataset, context);
-//     });
-//   });
